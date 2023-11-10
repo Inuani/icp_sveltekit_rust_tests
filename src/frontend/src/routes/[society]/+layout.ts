2c70e43546bf8bd988db6153a4fe01381d6c8a08
Elie Gauthey <elie.gauthey@gmail.com>
2023-11-10 19:28:25 +0100

    connection to [society] working

--- src/frontend/src/routes/[society]/+layout.ts
+++ src/frontend/src/routes/[society]/+layout.ts
@@ -1,8 +1,17 @@
 import { dS } from '$lib/stores';
 import { get } from 'svelte/store';
+import { Actor, HttpAgent } from '@dfinity/agent';
+import { host } from '$lib/constants';
+import { idlFactory } from '../../../../declarations/template/template.did'; 
 
 export async function load({ params }) {
 	const { society } = params;
 	const society_got = await get(dS).generator.get_society(society);
-	console.log(society_got);
+    const agent = new HttpAgent({ host });
+    agent.fetchRootKey().catch(console.error);
+    console.log(society_got);
+    const newCanisterActor = Actor.createActor(idlFactory, { agent, canisterId: society_got.Ok.canister_id });
+    const getName = await newCanisterActor.get_name();
+	console.log('soc:', getName);
+	
 }
